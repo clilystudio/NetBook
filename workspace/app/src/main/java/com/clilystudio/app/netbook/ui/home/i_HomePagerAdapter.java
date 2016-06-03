6a83a857606debc29ca71a0548f5948fb7b5982e
clilystudio <bullsgl@gmail.com>
2016-06-03 17:28:33 +0800

    Adapter Fixed

---
+++ workspace/app/src/main/java/com/clilystudio/app/netbook/ui/home/i_HomePagerAdapter.java
@@ -0,0 +1,64 @@
+package com.clilystudio.app.netbook.ui.home;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import java.util.List;
+
+final class i_HomePagerAdapter extends ZssqFragmentPagerAdapter {
+    private String[] tags = {"homeTag0", "homeTag1", "homeTag2"};
+    private HomeActivity activity;
+
+    public i_HomePagerAdapter(HomeActivity paramHomeActivity, FragmentManager paramFragmentManager) {
+        super(paramFragmentManager);
+        activity = paramHomeActivity;
+        List localList = activity.getFragmentList();
+
+        HomeShelfFragment localHomeShelfFragment = (HomeShelfFragment) activity.getSupportFragmentManager().findFragmentByTag(this.tags[0]);
+        if (localHomeShelfFragment == null) {
+            localHomeShelfFragment = HomeShelfFragment.b();
+        }
+        localList.add(localHomeShelfFragment);
+        HomeTopicFragment localHomeTopicFragment = (HomeTopicFragment) activity.getSupportFragmentManager().findFragmentByTag(this.tags[1]);
+        if (localHomeTopicFragment == null) {
+            localHomeTopicFragment = HomeTopicFragment.b();
+        }
+        localList.add(localHomeTopicFragment);
+        HomeFindFragment localHomeFindFragment = (HomeFindFragment) activity.getSupportFragmentManager().findFragmentByTag(this.tags[2]);
+        if (localHomeFindFragment == null) {
+            Bundle localBundle = new Bundle();
+            localBundle.putBoolean("game_center_show", false);
+            localHomeFindFragment = HomeFindFragment.a(localBundle);
+        }
+        localList.add(localHomeFindFragment);
+        FragmentTransaction localFragmentTransaction = paramFragmentManager.beginTransaction();
+        for (int i = 0;i < 3; i++) {
+            Fragment localFragment = (Fragment)localList.get(i);
+            if (!localFragment.isAdded()) {
+                localFragmentTransaction.add(activity.getmViewPager().getId(), localFragment, this.tags[i]);
+            }
+        }
+        if (!localFragmentTransaction.isEmpty()) {
+            localFragmentTransaction.commit();
+            paramFragmentManager.executePendingTransactions();
+        }
+    }
+
+    public final Fragment getFragment(int position) {
+        return (Fragment)activity.getFragmentList().get(position);
+    }
+
+    protected final String getTag(int position) {
+        return this.tags[position];
+    }
+
+    public final int getCount() {
+        return 3;
+    }
+
+    public final CharSequence getPageTitle(int paramInt) {
+        return activity.getResources().getStringArray(2131361797)[paramInt];
+    }
+}
