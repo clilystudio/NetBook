4e745bd41206aefb46e7046c15c45d31eab7d299
ShengGL <bullsgl@gmail.com>
2016-06-05 22:15:31 +0800

    R.java -> R_RelateBook.java

---
+++ workspace/app/src/main/java/com/clilystudio/app/netbook/util/R_RelateBook.java
@@ -0,0 +1,11 @@
+package com.clilystudio.app.netbook.util;
+
+import com.clilystudio.app.netbook.model.RelateBookRoot;
+
+public abstract class R_RelateBook extends S<RelateBookRoot> {
+}
+
+/* Location:           E:\10.Progs\Dev\Compiler\zssq.jar
+ * Qualified Name:     com.clilystudio.app.netbook.util.R
+ * JD-Core Version:    0.6.2
+ */
