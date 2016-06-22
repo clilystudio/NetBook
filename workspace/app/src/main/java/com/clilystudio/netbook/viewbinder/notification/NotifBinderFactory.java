04505af911120f0f7311a27bece15ecf6cbf37c7
clilystudio <bullsgl@gmail.com>
2016-06-22 11:05:52 +0800

    class NotifBinderFactory Fixed

--- workspace/app/src/main/java/com/clilystudio/netbook/viewbinder/notification/NotifBinderFactory.java
+++ workspace/app/src/main/java/com/clilystudio/netbook/viewbinder/notification/NotifBinderFactory.java
@@ -3,16 +3,14 @@ package com.clilystudio.netbook.viewbinder.notification;
 import com.clilystudio.netbook.model.NotificationItem;
 
 
+import org.apache.commons.lang3.reflect.ConstructorUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+
 import java.lang.reflect.InvocationTargetException;
 
 public class NotifBinderFactory {
     private static final Class[] mNotifBinders = new Class[]{PostReplyBinder.class, PostDistillateBinder.class, PostPushBinder.class, LinkPushBinder.class, CommentReplyBinder.class, PostHotBinder.class, TweetComment.class, TweetCommentRely.class, CommentLikeBinder.class, PostLikeBinder.class, PostVoteBinder.class, RetweetBinder.class, FollowBinder.class};
 
-    /*
-     * Enabled aggressive block sorting
-     * Enabled unnecessary exception pruning
-     * Enabled aggressive exception aggregation
-     */
     public static NotifBinder create(NotificationItem notificationItem) {
         if (notificationItem == null) {
             notificationItem = new NotificationItem();
@@ -24,17 +22,13 @@ public class NotifBinderFactory {
         while (n2 < n) {
             Class class_ = arrclass[n2];
             try {
-                if (((String) a.a(class_, "LABEL", false).get(new String())).equals(string)) {
-                    return (NotifBinder) a.a(class_, notificationItem);
+                 if (FieldUtils.getDeclaredField(class_, "LABEL", false).get("").equals(string)) {
+                    return (NotifBinder) ConstructorUtils.invokeConstructor(class_, (Object)notificationItem);
                 }
             } catch (IllegalAccessException var9_10) {
                 throw new RuntimeException("Label field must be set for every NotifBinder.");
-            } catch (NoSuchMethodException var8_8) {
+            } catch (NoSuchMethodException | InstantiationException | InvocationTargetException var8_8) {
                 var8_8.printStackTrace();
-            } catch (InstantiationException var7_7) {
-                var7_7.printStackTrace();
-            } catch (InvocationTargetException var6_6) {
-                var6_6.printStackTrace();
             }
             ++n2;
         }
