67707533f6a6f062b92d563a3977f79a60bd52ef
clilystudio <bullsgl@gmail.com>
2016-06-27 11:28:09 +0800

    package reader/dl fixed

--- workspace/app/src/main/java/com/clilystudio/netbook/reader/dl/BookDownloadService.java
+++ workspace/app/src/main/java/com/clilystudio/netbook/reader/dl/BookDownloadService.java
@@ -7,16 +7,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.IBinder;
-import com.clilystudio.netbook.am;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.clilystudio.netbook.MyApplication;
-import com.clilystudio.netbook.api.b;
+import com.clilystudio.netbook.am;
 import com.clilystudio.netbook.db.BookDlRecord;
 import com.clilystudio.netbook.db.BookReadRecord;
 import com.clilystudio.netbook.db.SourceRecord;
 import com.clilystudio.netbook.event.d;
-import com.clilystudio.netbook.event.i;
 import com.clilystudio.netbook.model.Chapter;
 import com.clilystudio.netbook.model.ChapterLink;
 import com.clilystudio.netbook.model.ChapterRoot;
@@ -24,8 +22,7 @@ import com.clilystudio.netbook.model.Toc;
 import com.clilystudio.netbook.model.TocSource;
 import com.clilystudio.netbook.model.TocSourceRoot;
 import com.clilystudio.netbook.util.I;
-import com.clilystudio.netbook.util.e;
-import com.squareup.a.l;
+import com.squareup.otto.Subscribe;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,11 +54,11 @@ public class BookDownloadService extends Service {
             bookDlRecord.setStatus(n);
             bookDlRecord.save();
         }
-        i.a().c(new com.clilystudio.netbook.event.I());
+        com.clilystudio.netbook.event.i.a().post(new com.clilystudio.netbook.event.I());
     }
 
     static /* synthetic */ void a(BookDownloadService bookDownloadService, int n) {
-        bookDownloadService.a(2);
+        bookDownloadService.a(n);
     }
 
     static /* synthetic */ void a(BookDownloadService bookDownloadService, BookReadRecord bookReadRecord) {
@@ -73,8 +70,8 @@ public class BookDownloadService extends Service {
     }
 
     static /* synthetic */ boolean a(BookDownloadService bookDownloadService, boolean bl) {
-        bookDownloadService.j = false;
-        return false;
+        bookDownloadService.j = bl;
+        return bl;
     }
 
     static /* synthetic */ ChapterLink[] a(BookDownloadService bookDownloadService, ChapterLink[] arrchapterLink) {
@@ -92,8 +89,8 @@ public class BookDownloadService extends Service {
     }
 
     static /* synthetic */ boolean b(BookDownloadService bookDownloadService, boolean bl) {
-        bookDownloadService.o = false;
-        return false;
+        bookDownloadService.o = bl;
+        return bl;
     }
 
     static /* synthetic */ int c(BookDownloadService bookDownloadService, int n) {
@@ -106,8 +103,8 @@ public class BookDownloadService extends Service {
     }
 
     static /* synthetic */ int d(BookDownloadService bookDownloadService, int n) {
-        bookDownloadService.c = 5;
-        return 5;
+        bookDownloadService.c = n;
+        return n;
     }
 
     static /* synthetic */ String d(BookDownloadService bookDownloadService) {
@@ -163,7 +160,7 @@ public class BookDownloadService extends Service {
     static /* synthetic */ void l(BookDownloadService bookDownloadService) {
         BookReadRecord bookReadRecord;
         if (bookDownloadService.a != null && !bookDownloadService.a.equals(bookDownloadService.m) && (bookReadRecord = BookReadRecord.get(bookDownloadService.a)) != null) {
-            String string = a.g(bookReadRecord.getReadMode());
+            String string = com.clilystudio.netbook.hpay100.a.a.g(bookReadRecord.getReadMode());
             String string2 = bookReadRecord.getDownloadedSource();
             if (string2 == null || !string2.contains(string)) {
                 bookReadRecord.setDownloadedSource(string2 + string);
@@ -282,8 +279,7 @@ public class BookDownloadService extends Service {
             @Override
             protected TocSourceRoot doInBackground(String... params) {
                 com.clilystudio.netbook.api.b.a();
-                TocSourceRoot tocSourceRoot = com.clilystudio.netbook.api.b.b().g(params[0]);
-                return tocSourceRoot;
+                return com.clilystudio.netbook.api.b.b().g(params[0]);
             }
 
             @Override
@@ -291,9 +287,8 @@ public class BookDownloadService extends Service {
                 super.onPostExecute(tocSourceRoot);
                 if (tocSourceRoot != null && tocSourceRoot.getSources() != null) {
                     TocSource[] arrtocSource = tocSourceRoot.getSources();
-                    int n = arrtocSource.length;
-                    for (int j = 0; j < n; ++j) {
-                        com.clilystudio.netbook.hpay100.a.a.a(arrtocSource[j], BookDownloadService.d(BookDownloadService.this));
+                    for (TocSource anArrtocSource : arrtocSource) {
+                        com.clilystudio.netbook.hpay100.a.a.a(anArrtocSource, BookDownloadService.d(BookDownloadService.this));
                     }
                 }
                 BookDownloadService.d(BookDownloadService.this, 5);
@@ -306,15 +301,15 @@ public class BookDownloadService extends Service {
                 }
                 BookDownloadService.o(BookDownloadService.this);
             }
-        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,  new String[]{this.a});
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, this.a);
     }
 
     private void b() {
         this.k = new com.clilystudio.netbook.reader.f(this.c);
-        boolean bl = a.h(this.c);
+        boolean bl = com.clilystudio.netbook.hpay100.a.a.h(this.c);
         String string = null;
         if (bl) {
-            String string2 = a.g(this.c);
+            String string2 = com.clilystudio.netbook.hpay100.a.a.g(this.c);
             SourceRecord sourceRecord = SourceRecord.get(this.a, string2);
             string = null;
             if (sourceRecord != null) {
@@ -323,19 +318,19 @@ public class BookDownloadService extends Service {
                 this.k.a(string, string3);
             }
         }
-        this.b = a.a(this.a, this.c, string, this.b);
+        this.b = com.clilystudio.netbook.hpay100.a.a.a(this.a, this.c, string, this.b);
         this.k.b(I.c);
         this.k.a(I.a);
         this.k.a(I.g);
         this.i.putExtra("SerDlStopFlag", 0);
-        this.h = a.j(this.a, this.b);
-        if (a.e() <= (long) (10 * this.e << 1)) {
-            com.clilystudio.netbook.util.e.a((Context) this, (String) "SD\u5361\u5269\u4f59\u5bb9\u91cf\u4e0d\u8db3\uff0c\u8bf7\u51cf\u5c11\u7f13\u5b58\u6570\u76ee\u6216\u589e\u52a0\u5b58\u50a8");
+        this.h = com.clilystudio.netbook.hpay100.a.a.j(this.a, this.b);
+        if (com.clilystudio.netbook.hpay100.a.a.e() <= (long) (10 * this.e << 1)) {
+            com.clilystudio.netbook.util.e.a(this, "SD卡剩余容量不足，请减少缓存数目或增加存储");
             this.stopSelf();
             return;
         }
         this.a(2);
-        i.a().c(new d(this.a, 2));
+        com.clilystudio.netbook.event.i.a().post(new d(this.a, 2));
         this.c();
     }
 
@@ -347,14 +342,14 @@ public class BookDownloadService extends Service {
         if (n < this.g.length && this.f <= this.e) {
             ChapterLink chapterLink = this.g[n];
             boolean bl = chapterLink.getUnreadble();
-            String string = am.e((String) chapterLink.getLink());
+            String string = am.e(chapterLink.getLink());
             while (bl || this.h.contains(string)) {
                 this.f = 1 + this.f;
                 n = this.d + this.f;
                 if (n < this.g.length) {
                     chapterLink = this.g[n];
                     bl = chapterLink.getUnreadble();
-                    string = am.e((String) chapterLink.getLink());
+                    string = am.e(chapterLink.getLink());
                     continue;
                 }
                 this.e();
@@ -385,13 +380,13 @@ public class BookDownloadService extends Service {
                     int n2 = com.clilystudio.netbook.hpay100.a.a.r(BookDownloadService.this);
                     if (BookDownloadService.j(BookDownloadService.this) == 1 && n2 > 1) {
                         BookDownloadService.k(BookDownloadService.this);
-                        com.clilystudio.netbook.util.e.a((Context) BookDownloadService.this.getApplicationContext(), (String) "\u6d41\u91cf\u4e0b\u81ea\u52a8\u6682\u505c\u7f13\u5b58\uff0c\u8fde\u63a5 Wi-Fi \u7ee7\u7eed\u6216\u624b\u52a8\u5f00\u59cb\u7f13\u5b58");
+                        com.clilystudio.netbook.util.e.a(BookDownloadService.this.getApplicationContext(), "流量下自动暂停缓存，连接 Wi-Fi 继续或手动开始缓存");
                     } else if (com.clilystudio.netbook.hpay100.a.a.t(BookDownloadService.this)) {
                         BookDownloadService.e(BookDownloadService.this);
                         BookDownloadService.b(BookDownloadService.this, false);
                     } else {
                         BookDownloadService.k(BookDownloadService.this);
-                        com.clilystudio.netbook.util.e.a((Context) BookDownloadService.this.getApplicationContext(), (String) "\u7f13\u5b58\u6682\u505c\uff0c\u8fde\u63a5\u7f51\u7edc\u540e\u7ee7\u7eed\u4e0b\u8f7d");
+                        com.clilystudio.netbook.util.e.a(BookDownloadService.this.getApplicationContext(), "缓存暂停，连接网络后继续下载");
                     }
                     BookDownloadService.b(BookDownloadService.this, n2);
                     if (chapterRoot != null && chapterRoot.getChapter() != null && (chapter = chapterRoot.getChapter()).getBody() != null) {
@@ -401,13 +396,12 @@ public class BookDownloadService extends Service {
                         if (BookDownloadService.m(BookDownloadService.this) == null) {
                             BookDownloadService.a(BookDownloadService.this, I.c);
                         }
-                        com.clilystudio.netbook.hpay100.a.a.a(BookDownloadService.d(BookDownloadService.this), BookDownloadService.m(BookDownloadService.this), am.e((String) string), chapter);
+                        com.clilystudio.netbook.hpay100.a.a.a(BookDownloadService.d(BookDownloadService.this), BookDownloadService.m(BookDownloadService.this), am.e(string), chapter);
                     }
                     BookDownloadService.c(BookDownloadService.this);
                     if (BookDownloadService.n(BookDownloadService.this) == 0 || BookDownloadService.g(BookDownloadService.this) == BookDownloadService.h(BookDownloadService.this)) {
                         com.clilystudio.netbook.event.i.a().post(new com.clilystudio.netbook.event.I());
                         BookDownloadService.c(BookDownloadService.this, BookDownloadService.g(BookDownloadService.this));
-                        return;
                     } else {
                         int nx = 1;
                         if (BookDownloadService.h(BookDownloadService.this) > 20) {
@@ -417,7 +411,6 @@ public class BookDownloadService extends Service {
                         {
                             com.clilystudio.netbook.event.i.a().post(new com.clilystudio.netbook.event.I());
                             BookDownloadService.c(BookDownloadService.this, BookDownloadService.g(BookDownloadService.this));
-                            return;
                         }
                     }
                 }
@@ -432,7 +425,7 @@ public class BookDownloadService extends Service {
         this.h();
         this.a(3);
         this.a();
-        i.a().c(new com.clilystudio.netbook.event.I());
+        com.clilystudio.netbook.event.i.a().post(new com.clilystudio.netbook.event.I());
     }
 
     private void e() {
@@ -466,7 +459,7 @@ public class BookDownloadService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        i.a().a(this);
+        com.clilystudio.netbook.event.i.a().register(this);
         this.i = new Intent("com.clilystudio.netbook.dlReceiver");
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.wifi.supplicant.STATE_CHANGE");
@@ -477,18 +470,18 @@ public class BookDownloadService extends Service {
         intentFilter.addAction("android.net.wifi.PICK_WIFI_NETWORK");
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         intentFilter.setPriority(1000);
-        this.registerReceiver(new BookDownloadService$MyNetworkMonitor(this), intentFilter);
+        this.registerReceiver(new BookDownloadService.MyNetworkMonitor(this), intentFilter);
         this.a();
     }
 
     @Override
     public void onDestroy() {
         this.j = true;
-        i.a().b(this);
+        com.clilystudio.netbook.event.i.a().unregister(this);
         super.onDestroy();
     }
 
-    @l
+    @Subscribe
     public void onDownloadStatus(d d2) {
         switch (d2.a()) {
             default: {
@@ -510,7 +503,7 @@ public class BookDownloadService extends Service {
         intentFilter.addAction("android.net.wifi.PICK_WIFI_NETWORK");
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         intentFilter.setPriority(1000);
-        this.registerReceiver(new BookDownloadService$MyNetworkMonitor(this), intentFilter);
+        this.registerReceiver(new BookDownloadService.MyNetworkMonitor(this), intentFilter);
         return super.onStartCommand(intent, n, n2);
     }
 
@@ -523,12 +516,12 @@ public class BookDownloadService extends Service {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (BookDownloadService.a(this.a) && a.r(this.a) == 1) {
+            if (BookDownloadService.a(this.a) && com.clilystudio.netbook.hpay100.a.a.r(this.a) == 1) {
                 BookDownloadService.b(this.a).putExtra("SerDlStopFlag", 0);
                 BookDownloadService.c(this.a);
                 BookDownloadService.a(this.a, false);
                 BookDownloadService.a(this.a, 2);
-                com.clilystudio.netbook.event.i.a().c(new d(BookDownloadService.d(this.a), 2));
+                com.clilystudio.netbook.event.i.a().post(new d(BookDownloadService.d(this.a), 2));
                 BookDownloadService.e(this.a);
                 BookDownloadService.b(this.a, false);
             }
