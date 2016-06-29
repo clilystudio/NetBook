fbb2dc98d0015818a282cc28367ee383c9ca8b71
clilystudio <bullsgl@gmail.com>
2016-06-29 11:36:54 +0800

    remove package ui/post

--- workspace/app/src/main/java/com/clilystudio/netbook/ui/post/AddTopicActivity.java
+++
@@ -1,8 +0,0 @@
-package com.clilystudio.netbook.ui.post;
-
-public class AddTopicActivity extends AddVoteActivity {
-    @Override
-    protected final String b() {
-        return "\u8bdd\u9898";
-    }
-}
--- workspace/app/src/main/java/com/clilystudio/netbook/ui/post/ax.java
+++
@@ -1,5 +0,0 @@
-package com.clilystudio.netbook.ui.post;
-
-public interface ax {
-    public void a();
-}
