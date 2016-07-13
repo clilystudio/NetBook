c1ea26ee2d86283948f70dd79ff9439c166a0cdd
clilystudio <bullsgl@gmail.com>
2016-07-13 10:13:22 +0800

    move class
    onPostCountChangeListener -> ag inner

--- workspace/app/src/main/java/com/clilystudio/netbook/util/ag.java
+++ workspace/app/src/main/java/com/clilystudio/netbook/util/ag.java
@@ -3,10 +3,12 @@ package com.clilystudio.netbook.util;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HandlerThread;
 import android.os.Message;
 
 import com.clilystudio.netbook.api.ApiService;
 import com.clilystudio.netbook.api.ApiServiceProvider;
+import com.clilystudio.netbook.model.PostCountRoot;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -90,5 +92,29 @@ public final class ag {
 
     public interface onPostCountChangeListener {
         void a(String var1, int var2);
+    }
+
+    static final class PostCountWorker extends HandlerThread implements Handler.Callback {
+        public PostCountWorker(String name) {
+            super(name);
+        }
+
+        @Override
+        public final boolean handleMessage(Message message) {
+            String string = message.getData().getString("bookId");
+            try {
+                PostCountRoot postCountRoot = ag.c().c(string);
+                Message message2 = Message.obtain(ag.d());
+                Bundle bundle = new Bundle();
+                bundle.putInt("postCount", postCountRoot.getPostCount());
+                bundle.putString("bookId", string);
+                message2.setData(bundle);
+                ag.d().sendMessage(message2);
+                return true;
+            } catch (Exception var3_6) {
+                var3_6.printStackTrace();
+                return false;
+            }
+        }
     }
 }
