58b46b7e55f3e0db0a3bdd4ae5aba0b2fdcc3119
ShengGL <bullsgl@gmail.com>
2016-07-17 12:50:16 +0800

    Fix Class SplashActivity & IntroActivity

--- workspace/app/src/main/java/com/clilystudio/netbook/ui/IntroActivity.java
+++ workspace/app/src/main/java/com/clilystudio/netbook/ui/IntroActivity.java
@@ -17,69 +17,24 @@ import com.clilystudio.netbook.ui.user.AuthLoginActivity;
 
 import java.util.ArrayList;
 
-public class IntroActivity extends Activity implements ViewPager.OnPageChangeListener,
-        View.OnClickListener {
-    private static final int[] f = new int[]{R.drawable.bg_intro_1, R.drawable.bg_intro_2};
-    private ViewPager a;
-    private PagerAdapter b;
-    private Button c;
-    private Button d;
-    private ArrayList<View> e;
-    private ImageView[] g;
-    private int h;
-    private boolean i = false;
-
-    static /* synthetic */ ArrayList a(IntroActivity introActivity) {
-        return introActivity.e;
-    }
-
-    private static void a(ImageView imageView, boolean bl) {
-        if (bl) {
-            imageView.setImageResource(R.drawable.ic_point_select);
-            return;
-        }
-        imageView.setImageResource(R.drawable.ic_point_normal);
-    }
-
-    private void a() {
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(-1, -1);
-        for (int i = 0; i < 2; ++i) {
-            ImageView imageView = new ImageView(this);
-            imageView.setLayoutParams(layoutParams);
-            imageView.setImageResource(f[i]);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            this.e.add(imageView);
-        }
-        this.a.setAdapter(this.b);
-        this.a.addOnPageChangeListener(this);
-        this.b();
-    }
-
-    private void b() {
-        LinearLayout linearLayout = (LinearLayout) this.findViewById(R.id.linear_bottom_dots);
-        this.g = new ImageView[2];
-        for (int i = 0; i < 2; ++i) {
-            this.g[i] = (ImageView) linearLayout.getChildAt(i);
-            IntroActivity.a(this.g[i], false);
-            this.g[i].setTag(i);
-        }
-        this.h = 0;
-        IntroActivity.a(this.g[this.h], true);
-    }
-
-    private void c() {
-        this.startActivity(new Intent(this, HomeActivity.class));
-        this.finish();
-    }
+public class IntroActivity extends Activity implements ViewPager.OnPageChangeListener, View.OnClickListener {
+    private static final int[] mImageResIds = new int[]{R.drawable.bg_intro_1, R.drawable.bg_intro_2};
+    private Button mQuickLoginButton;
+    private Button mEntryAppButton;
+    private ArrayList<View> mImageViewList;
+    private ImageView[] mIndicatorImageView;
+    private int mCurrPage;
+    private boolean mIsStop = false;
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnQuickLogin:
-                this.startActivity(AuthLoginActivity.a(this));
+                startActivity(AuthLoginActivity.a(this));
                 break;
             case R.id.btnEntryApp:
-                this.c();
+                startActivity(new Intent(this, HomeActivity.class));
+                finish();
                 break;
         }
     }
@@ -87,25 +42,25 @@ public class IntroActivity extends Activity implements ViewPager.OnPageChangeLis
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        this.setContentView(R.layout.splash_intro);
-        this.e = new ArrayList<>();
-        this.a = (ViewPager) this.findViewById(R.id.vp_intro);
-        this.b = new PagerAdapter() {
+        setContentView(R.layout.splash_intro);
+        mImageViewList = new ArrayList<>();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_intro);
+        PagerAdapter pagerAdapter = new PagerAdapter() {
 
             @Override
             public int getCount() {
-                return IntroActivity.a(IntroActivity.this).size();
+                return mImageViewList.size();
             }
 
             @Override
             public Object instantiateItem(ViewGroup container, int position) {
-                container.addView((View) IntroActivity.a(IntroActivity.this).get(position), 0);
-                return IntroActivity.a(IntroActivity.this).get(position);
+                container.addView(mImageViewList.get(position), 0);
+                return mImageViewList.get(position);
             }
 
             @Override
             public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView((View) IntroActivity.a(IntroActivity.this).get(position));
+                container.removeView(mImageViewList.get(position));
             }
 
             @Override
@@ -113,11 +68,29 @@ public class IntroActivity extends Activity implements ViewPager.OnPageChangeLis
                 return view == object;
             }
         };
-        this.c = (Button) this.findViewById(R.id.btnQuickLogin);
-        this.c.setOnClickListener(this);
-        this.d = (Button) this.findViewById(R.id.btnEntryApp);
-        this.d.setOnClickListener(this);
-        this.a();
+        mQuickLoginButton = (Button) findViewById(R.id.btnQuickLogin);
+        mQuickLoginButton.setOnClickListener(this);
+        mEntryAppButton = (Button) findViewById(R.id.btnEntryApp);
+        mEntryAppButton.setOnClickListener(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        for (int resId : mImageResIds) {
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(layoutParams);
+            imageView.setImageResource(resId);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            mImageViewList.add(imageView);
+        }
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.addOnPageChangeListener(this);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_bottom_dots);
+        mIndicatorImageView = new ImageView[2];
+        for (int i = 0; i < mIndicatorImageView.length; ++i) {
+            mIndicatorImageView[i] = (ImageView) linearLayout.getChildAt(i);
+            mIndicatorImageView[i].setImageResource(R.drawable.ic_point_normal);
+            mIndicatorImageView[i].setTag(i);
+        }
+        mCurrPage = 0;
+        mIndicatorImageView[mCurrPage].setImageResource(R.drawable.ic_point_select);
     }
 
     @Override
@@ -129,33 +102,34 @@ public class IntroActivity extends Activity implements ViewPager.OnPageChangeLis
     }
 
     @Override
-    public void onPageSelected(int n) {
-        if (n < 0 || n > 1 || this.h == n) {
+    public void onPageSelected(int position) {
+        if (position < 0 || position > 1 || mCurrPage == position) {
             return;
         }
-        IntroActivity.a(this.g[n], true);
-        IntroActivity.a(this.g[this.h], false);
-        this.h = n;
-        if (this.h == 0) {
-            this.d.setTextColor(this.getResources().getColor(R.color.intro_blue_color));
-            this.c.setTextColor(this.getResources().getColor(R.color.intro_blue_color));
-            return;
+        mIndicatorImageView[position].setImageResource(R.drawable.ic_point_select);
+        mIndicatorImageView[mCurrPage].setImageResource(R.drawable.ic_point_normal);
+        mCurrPage = position;
+        if (mCurrPage == 0) {
+            mEntryAppButton.setTextColor(getResources().getColor(R.color.intro_blue_color));
+            mQuickLoginButton.setTextColor(getResources().getColor(R.color.intro_blue_color));
+        } else {
+            mEntryAppButton.setTextColor(getResources().getColor(R.color.intro_red_color));
+            mQuickLoginButton.setTextColor(getResources().getColor(R.color.intro_red_color));
         }
-        this.d.setTextColor(this.getResources().getColor(R.color.intro_red_color));
-        this.c.setTextColor(this.getResources().getColor(R.color.intro_red_color));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (this.i) {
-            this.c();
+        if (mIsStop) {
+            startActivity(new Intent(this, HomeActivity.class));
+            finish();
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        this.i = true;
+        mIsStop = true;
     }
 }
--- workspace/app/src/main/java/com/clilystudio/netbook/ui/SplashActivity.java
+++ workspace/app/src/main/java/com/clilystudio/netbook/ui/SplashActivity.java
@@ -9,64 +9,34 @@ import com.clilystudio.netbook.R;
 import com.clilystudio.netbook.ui.home.HomeActivity;
 import com.clilystudio.netbook.util.CommonUtil;
 
-import java.util.Calendar;
-
 public class SplashActivity extends Activity {
-    private Handler mHandler = new Handler();
-    private boolean e = false;
-    private boolean f = false;
-
-    public final void d() {
-        Intent intent;
-        if (this.e) {
-            return;
-        }
-        if (!CommonUtil.isLogined()) {
-            intent = new Intent(this, IntroActivity.class);
-        } else {
-            intent = new Intent(this, HomeActivity.class);
-        }
-        this.startActivity(intent);
-        this.finish();
-    }
+    private static final int DELAY_MILLIS = 1200;
+    private boolean mIsStop = false;
 
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        this.setContentView(R.layout.splash);
-        this.mHandler.postDelayed(new Runnable() {
+        setContentView(R.layout.splash);
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                SplashActivity.this.d();
+                if (!mIsStop) {
+                    Intent intent;
+                    if (!CommonUtil.isLogined()) {
+                        intent = new Intent(SplashActivity.this, IntroActivity.class);
+                    } else {
+                        intent = new Intent(SplashActivity.this, HomeActivity.class);
+                    }
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }, 1200);
+        }, DELAY_MILLIS);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        this.e = true;
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        if (!this.f) return;
-        if (this.hasWindowFocus() || this.f) {
-            this.d();
-            return;
-        }
-        this.f = true;
-    }
-
-    @Override
-    public void onResume() {
-        int n;
-        Calendar calendar = Calendar.getInstance();
-        super.onResume();
-        if (!CommonUtil.isLogined() || (n = 10000 * calendar.get(Calendar.YEAR) + 100 * calendar.get(Calendar.MONTH) + calendar.get(Calendar.DATE)) <= CommonUtil.getIntPref(this, "KEY_OPEN_TIME", 0) || CommonUtil.getAccount().getUser().getGender() == null) {
-            return;
-        }
-        CommonUtil.putIntPref(this, "KEY_OPEN_TIME", n);
+        this.mIsStop = true;
     }
 }
